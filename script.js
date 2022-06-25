07ad84f774f571ef03de989f04a4f64ba697fe82
Влад <vlad@MacBook-Pro-Vlad.local>
2022-06-25 19:48:25 +0300

    make short script

---
+++ script.js
@@ -0,0 +1,15 @@
+function main (a = 3, b = 3,func) {
+    let result =sum (a,b);
+	return typeof func === "function"? func(result): result;
+}
+
+function sum (a, b) { 
+        return a + b; 
+    }
+	
+function mult(x) {
+	return x *= 100;
+}
+    
+console.log(main(4,3,mult))
+
