2b34908bd3affe49e0b28c5fcd977f3b27211971
Влад <vlad@MacBook-Pro-Vlad.local>
2022-06-25 20:10:55 +0300

    fix

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function main (a = 3, b = 3,func) {
-    let result =sum (a,b);
+function main(a = 3, b = 3, func) {
+    let result =sum(a, b);
 	return typeof func === "function"? func(result): result;
 }
 
-function sum (a, b) { 
+function sum(a, b) { 
         return a + b; 
     }
-	
+
 function mult(x) {
 	return x *= 100;
 }
